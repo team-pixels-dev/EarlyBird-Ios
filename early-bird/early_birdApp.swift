be2ede1d3f9790a6234fcf119a66605f6bf8374d
jayou2323 <jayou1223@gmail.com>
2025-02-12 22:27:45 +0900

    Initial Commit

---
+++ early-bird/early_birdApp.swift
@@ -0,0 +1,17 @@
+//
+//  early_birdApp.swift
+//  early-bird
+//
+//  Created by JAYOU KOO on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct early_birdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
