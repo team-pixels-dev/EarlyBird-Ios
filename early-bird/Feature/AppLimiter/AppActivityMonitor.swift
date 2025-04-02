2f0395d6ea07df4209fffba1bd403a68da2aeaaa
jayou2323 <jayou1223@gmail.com>
2025-04-02 14:49:08 +0900

    us, uk 알림 문구 수정

--- early-bird/Feature/AppLimiter/AppActivityMonitor.swift
+++ early-bird/Feature/AppLimiter/AppActivityMonitor.swift
@@ -7,9 +7,16 @@
 import DeviceActivity
 import ManagedSettings
 
-
 class AppActivityMonitor: DeviceActivityMonitor {
     override func intervalDidEnd(for activity: DeviceActivityName) {
-        AppLimiter.shared.stopBlocking()
+        super.intervalDidEnd(for: activity)
+
+        if activity.rawValue == "GlobalAppLimit" {
+            let store = ManagedSettingsStore()
+            store.shield.applications = []
+            store.shield.applicationCategories = ShieldSettings.ActivityCategoryPolicy.none
+            print("✅ [Extension] 앱 차단 해제 완료 (intervalDidEnd)")
+        }
+        // Handle the end of the interval.
     }
 }
